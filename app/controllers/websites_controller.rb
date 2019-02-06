273bc2ed011adf407b073649553f64c07325e279
Alexander Haumer <alexander.haumer@me.com>
2019-02-06 15:17:13 +0000

    views and controllers for website working

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,4 +2,9 @@ class WebsitesController < ApplicationController
   def index
     @websites = Website.all
   end
+
+  def show
+    @website = Website.find(params[:id])
+    @jobs = @website.jobs
+  end
 end
