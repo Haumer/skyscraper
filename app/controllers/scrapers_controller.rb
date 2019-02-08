67553a725050b64813d173e4f115c2aa48494e2a
Alexander Haumer <alexander.haumer@me.com>
2019-02-08 11:16:03 +0000

    troubleshooting nokogiri

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -1,5 +1,3 @@
-require "nokogiri"
-require "open-uri"
 class ScrapersController < ApplicationController
   def index
     @scrapers = Scraper.all
--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -1,5 +1,5 @@
-require "nokogiri"
-require "open-uri"
+# require "nokogiri"
+# require "open-uri"
 
 class ScrapeJob < ApplicationJob
   queue_as :default
