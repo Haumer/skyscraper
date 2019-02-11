9082189b9f23e670cb7f80fd95025cbf8593754b
Alexander Haumer <alexander.haumer@me.com>
2019-02-11 01:02:45 +0000

    search history group my job | created db.xml

--- app/controllers/search_histories_controller.rb
+++ app/controllers/search_histories_controller.rb
@@ -2,5 +2,11 @@ class SearchHistoriesController < ApplicationController
   def index
     @search_histories = SearchHistory.where(user_id: current_user.id)
     @search_histories_ordered = @search_histories.order(created_at: :desc)
+    @grouped_by_job = group_by_job
+  end
+
+  def group_by_job
+    @search_history = SearchHistory.where(id: 7).first
+    @search_history.search.jobs.group_by { |job| job.website }
   end
 end
